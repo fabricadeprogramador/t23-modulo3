33826fd49d1209febfbd27d501da1514d2003b07
Vinicius Souza <viniciuspodi@gmail.com>
2019-04-08 21:30:15 -0400

    Criando template inicial do App

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <NavBar />
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-footer class="pa-2 primary white--text">
+      <v-spacer></v-spacer>
+      <div>Fábrica de Programador - T23 &copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+  import NavBar from '@/components/NavBar'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  export default {
+    name: 'App',
+    components: {
+      NavBar
+    },
+    data() {
+      return {
+        //
+      }
+    }
+  }
+</script>
--- src/router.js
+++ src/router.js
@@ -1,23 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Produtos from './views/Produtos.vue'
+import Carrinho from './views/Carrinho.vue'
+import Compras from './views/Compras.vue'
+import Perfil from './views/Perfil.vue'
+import Pagamento from './views/Pagamento.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "Produtos",
+      component: Produtos
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: "/carrinho",
+      name: "Carrinho",
+      component: Carrinho
+    },
+    {
+      path: "/compras",
+      name: "Compras",
+      component: Compras
+    },
+    {
+      path: "/perfil",
+      name: "Perfil",
+      component: Perfil
+    },
+    {
+      path: "/pagamento",
+      name: "Pagamento",
+      component: Pagamento
     }
   ]
 })
