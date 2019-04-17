2366983badf7fb3ca8c910e5b8893f88f8c8da26
Vinicius Souza <viniciuspodi@gmail.com>
2019-04-17 19:38:11 -0400

    Corrigindo corte de imagem em produtos

--- src/views/Produtos.vue
+++ src/views/Produtos.vue
@@ -1,65 +1,68 @@
 <template>
-  <v-layout class="my-4" justify-center row wrap>
-    
-    <!-- Cards com os Produtos -->
-    <v-flex class="ma-2" v-for="produto in produtos" :key="produto.nome" align-start row xs12 sm6 md4 lg3-sm3>
-      <v-card class="text-xs-center">
-        
-        <v-img :src="produto.imagem" aspect-ratio="1" width="400px"></v-img>
+  <v-container grid-list-sm fluid>
+    <v-layout class="my-4" justify-center row wrap>
 
-        <v-card-title primary-title>
-          <div>
-            <h3>{{produto.descricao}}</h3>
-            <h5 class="text-xs-left">R$ {{ produto.valor}}</h5>
-          </div>
-        </v-card-title>
+      <!-- Cards com os Produtos -->
+      <v-flex class="ma-2" v-for="produto in produtos" :key="produto.nome" align-start row xs12 sm6 md4 lg3-sm3>
+        <v-card class="text-xs-center">
 
-        <v-card-text>
-          <div class="subheading text-xs-left">{{ produto.marca }}</div>
-        </v-card-text>
+          <v-card flat tile class="d-flex ma-3">
+            <v-img :src="produto.imagem" contain> </v-img>
+          </v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3>{{produto.descricao}}</h3>
+              <h5 class="text-xs-left">R$ {{ produto.valor}}</h5>
+            </div>
+          </v-card-title>
 
-        <v-flex class="text-xs-right">
-          <v-card-actions>
+          <v-card-text>
+            <div class="subheading text-xs-left">{{ produto.marca }}</div>
+          </v-card-text>
+
+          <v-flex class="text-xs-right">
+            <v-card-actions>
               <v-spacer></v-spacer>
-              <v-icon  color="primary" dark @click="editar(props.item)" right>
+              <v-icon color="primary" dark @click="editar(props.item)" right>
                 search
               </v-icon>
-          </v-card-actions>
-        </v-flex>
+            </v-card-actions>
+          </v-flex>
 
-      </v-card>
-    </v-flex>
-    <!-- ////Cards com os Produtos -->
+        </v-card>
+      </v-flex>
+      <!-- ////Cards com os Produtos -->
 
-  </v-layout>
+    </v-layout>
+  </v-container>
 </template>
 
 
 <script>
-import HTTPRequest from '@/utils/HTTPRequests'
+  import HTTPRequest from '@/utils/HTTPRequests'
 
-export default {
+  export default {
 
-  data: () => ({
-    valid: false,
-    produtos: []
+    data: () => ({
+      valid: false,
+      produtos: []
 
-  }),
+    }),
 
-  mounted: function () {
-    this.buscarProdutos()
-  },
+    mounted: function () {
+      this.buscarProdutos()
+    },
 
-  methods: {
-    
-    buscarProdutos(){
-      HTTPRequest.buscarProdutos()
-        .then(produtos =>{
+    methods: {
+
+      buscarProdutos() {
+        HTTPRequest.buscarProdutos()
+          .then(produtos => {
             this.produtos = produtos
-        })
-    }
+          })
+      }
 
-  },
+    },
 
-}
+  }
 </script>
