bd5d531e55ba6c858cac71b98ae40ef2be6e731f
vinicius.souza <viniciuspodi@gmail.com>
2019-04-22 21:00:21 -0400

    Implementando regra de carrinho

--- src/views/Produtos.vue
+++ src/views/Produtos.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container grid-list-sm fluid>
     <v-layout class="my-4" justify-center row wrap>
-  
+
       <!-- Cards com os Produtos -->
       <v-flex class="ma-2" v-for="produto in produtos" :key="produto.nome" align-start row xs12 sm6 md4 lg3-sm3>
         <v-card class="text-xs-center">
-  
+
           <v-card flat tile class="d-flex ma-3">
             <v-img :src="produto.imagem" contain max-height="200px"> </v-img>
           </v-card>
@@ -15,11 +15,11 @@
               <h5 class="text-xs-left">R$ {{ produto.valor}}</h5>
             </div>
           </v-card-title>
-  
+
           <v-card-text>
             <div class="subheading text-xs-left">{{ produto.marca }}</div>
           </v-card-text>
-  
+
           <!-- inicio descrição do produto -->
           <v-flex class="text-xs-right">
             <v-card-actions>
@@ -29,92 +29,116 @@
               </v-icon>
             </v-card-actions>
           </v-flex>
-  
-          <v-layout row justify-center>
-            <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-              <template v-slot:activator="{ on }"></template>
-                <v-card>
-                  <v-toolbar color="grey darken-2 white--text">
-                    <v-btn icon dark @click="dialog = false">
-                      <v-icon>close</v-icon>
-                    </v-btn>                    
-                    <v-spacer></v-spacer>
-                    <v-toolbar-items></v-toolbar-items>
-                  </v-toolbar>
-                  
-                  <!-- inicio produto -->
-                  <v-card flat tile class="d-flex ma-3">
-                  <v-img :src="imagem" contain max-height="300px"> </v-img>
-                  </v-card>
-                  <v-card-title primary-title>
-                    <div>
-                      <h1>{{ descricao }}</h1>
-                      <h2 class="text-xs-left">R$ {{ valor }}</h2>
-                    </div>
-                  </v-card-title>
-  
-                  <v-card-text>
-                    <h2 class="subheading text-xs-left">{{ marca }}</h2>
-                  </v-card-text>
 
-                  <div>
-                  <v-btn color="grey darken-2 white--text">
-                    <v-icon>add_shopping_cart</v-icon>                                     
-                    Comprar</v-btn>
-                  </div>
-                  <!-- fim produto -->
 
-                </v-card>
-              </v-dialog>
-            </v-layout>
-          <!-- fim descrição do produto -->
         </v-card>
       </v-flex>
       <!-- ////Cards com os Produtos -->
-  
+
     </v-layout>
+
+    <v-layout row justify-center>
+      <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+
+        <v-card>
+          <v-toolbar color="grey darken-2 white--text">
+            <v-btn icon dark @click="dialog = false">
+              <v-icon>close</v-icon>
+            </v-btn>
+          </v-toolbar>
+
+          <v-layout>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-card>
+
+                <v-img class="white--text" height="200px" :src="produtoSelecionado.imagem" contain>
+                </v-img>
+
+                <v-card-title>
+                  <div>
+                    <span class="grey--text">{{produtoSelecionado.marca}}</span><br>
+                    <span>{{produtoSelecionado.descricao}}</span>
+                  </div>
+                </v-card-title>
+
+                <v-layout align-center justify-center column fill-height>
+                  <v-flex>
+                    <v-card-actions>
+                      <v-btn color="primary" @click="adicionarAoCarrinho()">Adicionar ao Carrinho</v-btn>
+                    </v-card-actions>
+                  </v-flex>
+                </v-layout>
+              </v-card>
+            </v-flex>
+          </v-layout>
+
+
+        </v-card>
+
+      </v-dialog>
+    </v-layout>
+    <!-- fim descrição do produto -->
   </v-container>
 </template>
 
 
 <script>
   import HTTPRequest from '@/utils/HTTPRequests'
-  
+
   export default {
-  
+
     data: () => ({
       dialog: false,
       produtos: [],
-      imagem: "",
-      descricao: "",
-      marca: "",
-      valor: "",
-  
+      produtoSelecionado: {}
     }),
-  
-    mounted: function() {
+
+    mounted: function () {
       this.buscarProdutos()
     },
-  
+
     methods: {
-  
+
       buscarProdutos() {
         HTTPRequest.buscarProdutos()
           .then(produtos => {
             this.produtos = produtos
           })
       },
-  
+
       exibirDetalhes(produto) {
         this.dialog = true
-        this.valor = produto.valor
-        this.descricao = produto.descricao
-        this.marca = produto.marca
-        this.valor = produto.valor
-        this.imagem = produto.imagem
+        this.produtoSelecionado = produto
+      },
+
+      adicionarAoCarrinho() {
+        let carrinho = localStorage.getItem("carrinho")
+        if (carrinho == null) {
+          let produtos = []
+          produtos.push(this.produtoSelecionado)
+
+          localStorage.setItem("carrinho", JSON.stringify(produtos))
+        } else {
+          let produtos = JSON.parse(localStorage.getItem("carrinho"))
+          let jaExiste = false
+
+          for (let i = 0; i < produtos.length; i++) {
+            if (produtos[i]._id == this.produtoSelecionado._id) {
+              jaExiste = true
+            }
+          }
+
+          if (!jaExiste) {
+            produtos.push(this.produtoSelecionado)
+            localStorage.setItem("carrinho", JSON.stringify(produtos))
+          } else {
+            alert("Produto já adicionado ao carrinho!")
+          }
+
+        }
       }
-  
+
     },
-  
+
   }
 </script>
